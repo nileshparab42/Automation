b45d36cd065934da6ce8f010a5a847c2d6ffae7a
Nilesh Parab <nileshparab5623@gmail.com>
2022-02-05 13:30:13 +0530

    Added comments

--- insta-liker.py
+++ insta-liker.py
@@ -1,21 +1,29 @@
 # Resolution of display = 1366 X 768
 # Browser : Firefox
 
+#----------------------importing relevent modules----------------------------
 import pyautogui as pg
 import time as t
 
+#---------------Taking insta id and number of posts from users---------------
 insta_id = input("Enter insta id : ")
 post_queue = int(input("How many post do you want to Like ? : "))
+
+#---------------------Searching for the insta id-----------------------------
 t.sleep(3)
 pg.moveTo(461,63)
 pg.leftClick()
 pg.write("https://www.instagram.com/"+insta_id)
 pg.press("enter")
+
+#-----------------------Selecting first post---------------------------------
 t.sleep(8)
 pg.moveTo(910,333)
 pg.scroll(-700)
 pg.moveTo(350,480)
 pg.leftClick()
+
+#-------------------Auto loop for scrolling posts----------------------------
 t.sleep(2)
 for i in range(post_queue):
     pg.moveTo(429,419)
@@ -24,3 +32,4 @@ for i in range(post_queue):
     pg.leftClick()
     t.sleep(2)
 
+#-------------------------End of the program---------------------------------
